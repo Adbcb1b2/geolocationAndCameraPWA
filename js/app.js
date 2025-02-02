ea8e43be0f71abdd90f20534198fa4255009e79e
Kim Richards <richards.kimc@gmail.com>
2025-02-02 20:13:04 +0000

    Updated paths to remote paths

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-// If the browser has service worker capability
-if('serviceWorker' in navigator){
-    // Wait for page to load before registering service worker
+// Registering the service worker
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        // Register the service worler
-        navigator.serviceWorker.register('https://mi-linux.wlv.ac.uk/~2111532/PWA/service-worker.html')
-        .then(registration => {
-            console.log('Service Worker registered');
-        })
-        .catch(error => {
-            console.log('Failed to register service worker');
-        });
-
+        navigator.serviceWorker.register('/~2111532/PWA/geolocationAndCameraPWA/service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered');
+            })
+            .catch(error => {
+                console.log('Failed to register service worker', error);
+            });
     });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,24 @@
 // Define cache name and which files to cache
-const CACHE_NAME = "geo-cam-pwa"
+const CACHE_NAME = "geo-cam-pwa";
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/css/styles.css",
-    "/pages/camera.html",
-    "/pages/geolocation.html",
-    "/js/camera.js",
-    "/js/geolocation.js",
-    "/js/app.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/~2111532/PWA/geolocationAndCameraPWA/index.html", 
+    "/~2111532/PWA/geolocationAndCameraPWA/css/styles.css", 
+    "/~2111532/PWA/geolocationAndCameraPWA/pages/camera.html", 
+    "/~2111532/PWA/geolocationAndCameraPWA/pages/geolocation.html", 
+    "/~2111532/PWA/geolocationAndCameraPWA/js/camera.js", 
+    "/~2111532/PWA/geolocationAndCameraPWA/js/geolocation.js", 
+    "/~2111532/PWA/geolocationAndCameraPWA/js/app.js", 
+    "/~2111532/PWA/geolocationAndCameraPWA/icons/icon-192x192.png", 
+    "/~2111532/PWA/geolocationAndCameraPWA/icons/icon-512x512.png"
 ];
 
 // Cache items when service worker is installed
 self.addEventListener("install", (event) => {
-    // Wait until cache is populated before completing istall
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            // Add all the files to the cache
-            return cache.addAll(FILES_TO_CACHE);
+            return cache.addAll(FILES_TO_CACHE).catch((error) => {
+                console.error('Failed to cache:', error);
+            });
         })
     );
 });
