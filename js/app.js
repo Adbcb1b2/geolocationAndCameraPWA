8aecd3d6559e289dbc5c75c3c1dd95a755fd8215
Kim Richards <richards.kimc@gmail.com>
2025-02-02 18:07:13 +0000

    Changed file paths in manifest and app js to remote URLs.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ if('serviceWorker' in navigator){
     // Wait for page to load before registering service worker
     window.addEventListener('load', () => {
         // Register the service worler
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('https://mi-linux.wlv.ac.uk/~2111532/PWA/service-worker.html')
         .then(registration => {
             console.log('Service Worker registered');
         })
