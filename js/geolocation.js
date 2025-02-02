0bf94388c00eacc89db17700e89de15697c0d210
Kim Richards <richards.kimc@gmail.com>
2025-02-02 16:48:16 +0000

    Implemented geolocation functionality

---
+++ js/geolocation.js
@@ -0,0 +1,42 @@
+// Declare UI elements
+const getLocationButton = document.getElementById("getLocation");
+const locationText = document.getElementById("location");
+
+// When 'Get Location' button is clicked, get current location and display in HTML paragraph
+getLocationButton.addEventListener("click", () => {
+    if("geolocation" in navigator){
+        // Use navigator object to get current location
+        navigator.geolocation.getCurrentPosition(
+            // If successful, display data in HTML paragraph
+            (position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const accuracy = position.coords.accuracy;
+                locationText.innerHTML = `Latitude: ${latitude} <br> Longitude: ${longitude} <br> Accuracy: ${accuracy}m`;
+            }, 
+            // If unsuccessful, log the error and alert user
+            (error) => {
+                console.error("Error:", error);
+                navigator.permissions.query({name: "geolocation"}).then((result) => {
+                    if(result.state === "granted"){
+                        alert("Cannot get location.", error);
+                        locationText.innerHTML = "Unable to get location."
+                    }else if (result.state === "denied"){
+                        alert("Cannot get location - Permissions previously denied. Please change the settings in your browser.");
+                        locationText.innerHTML = "Location permissions previously denied. Please change the settings in your browser."
+                    }
+                })
+
+
+            },
+            // Request location with high accuracy
+            {
+                enableHighAccuracy: true // Optional
+            }
+        )
+
+    }else{
+        alert("Geolocation not supported. Please try a different browser.");
+        locationText.innerHTML("Geolocation not supported in this browser.")
+    }
+});
