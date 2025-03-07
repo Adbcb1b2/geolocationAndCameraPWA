e52466af15bba210c9529ec9bdf30945f8234d86
Kim Richards <richards.kimc@gmail.com>
2025-03-07 15:40:21 +0000

    Amended file structure

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Registering the service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/~2111532/PWA/geolocationAndCameraPWA/service-worker.js')
+        navigator.serviceWorker.register('/~2111532/PWA/geolocationAndCameraPWA/js/service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered');
             })
